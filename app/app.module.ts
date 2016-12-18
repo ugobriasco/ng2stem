9585e7d7ee6c38d858d4643e01d426f35928a9fc
Ugo Briasco <ugo.briasco@gmail.com>
2016-12-18 16:20:19 +0100

    added http module

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; //for using the banana in the box
 import { AppComponent } from './app.component';
+import { HttpModule} from '@angular/http';
 
 
 
 @NgModule({
-  imports: [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent], //declare all the components in use!
+  imports: [ BrowserModule, FormsModule, HttpModule ],
+  declarations: [ AppComponent ], //declare all the components in use!
   bootstrap: [ AppComponent ] //starting point
 })
 export class AppModule {}
